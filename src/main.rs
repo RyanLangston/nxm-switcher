7157474e7b8426c9553509bc5c8b563826969bba
Ryan Langston <dragonlord100587@gmail.com>
2025-05-15 11:49:13 -0600

    More code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
 // Work on this
 // TODO: DO it
 
+use clap::{Parser, Subcommand, command};
+use serde::Deserialize;
+use std::process::Command;
+
+const MIME_TYPES: &[&str] = &["x-scheme-handler/nxm", "x-scheme-handler/nxm-protocol"];
+
+#[derive(Deserialize)]
+struct Handler {
+    name: String,
+    // TODO: Make this its own type
+    file: String,
+}
+
+/// CLI Arguments that it'll accept
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Status,
+    Select,
+}
+
 fn main() {
     println!("Placeholder")
 }
