68e054fb2c56ce759a836f255f301da44f468ddc
Ryan Langston <dragonlord100587@gmail.com>
2025-05-15 12:05:44 -0600

    Extra docs

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@ use clap::{Parser, Subcommand, command};
 use serde::Deserialize;
 use std::process::Command;
 
+/// The MIME Types
 const MIME_TYPES: &[&str] = &["x-scheme-handler/nxm", "x-scheme-handler/nxm-protocol"];
 
+/// Handles config related things
 #[derive(Deserialize)]
 struct Handler {
+    /// Name of the handler to be displayed to the user
     name: String,
     // TODO: Make this its own type
+    /// Path to the .desktop file
     file: String,
 }
 
@@ -22,9 +26,12 @@ struct Cli {
     command: Commands,
 }
 
+/// Possible Commands the user can use
 #[derive(Subcommand)]
 enum Commands {
+    /// Status of the mime
     Status,
+    /// Select the handler
     Select,
 }
 
